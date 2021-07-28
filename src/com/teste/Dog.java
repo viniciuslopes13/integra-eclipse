030955c74ad6ad34d5f1a82552f75d429dc284c3
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2021-07-28 11:56:38 -0300

    Add project

---
+++ src/com/teste/Dog.java
@@ -0,0 +1,26 @@
+package com.teste;
+
+public class Dog {
+		
+	private String nome;
+	private String dataNascimento;
+	
+	public Dog() {
+		super();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+}
